712fab554777c650627990e6c955bc6c03f5c1a7
Dhaval Parmar <ddhaval2655@gmail.com>
2026-02-16 14:56:36 +0530

    Delete pro5primenumber1to50.py

--- pro5primenumber1to50.py
+++
@@ -1,9 +0,0 @@
-print("Prime numbers between 1 and 50:")
-
-for num in range(2, 51):
-    for i in range(2, num):
-        if num % i == 0:
-            break
-    else:
-        print(num, end=" ")
- 
