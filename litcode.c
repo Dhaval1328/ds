98c25eddd48d898ee7a3aa66ecaf59dff937758b
Dhaval Parmar <ddhaval2655@gmail.com>
2026-03-17 10:53:19 +0530

    Update litcode.c

--- litcode.c
+++ litcode.c
@@ -10,7 +10,7 @@ struct ListNode* reverseBetween(struct ListNode* head , int left, int right) {
 
     struct ListNode *prev = NULL; last =head;
 
-    for(i=1; i<last; i++){
+    for(i=1; i<last; i++ ){
         prev = last;
         last = last ->next;
     }
